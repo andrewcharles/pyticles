86f32b7c3d720119bfcbb5d23fdda2e09ee406fe
ac1201 <ac1201@63705391-ac41-0410-8934-9f98adecfdbd>
2009-06-25 06:54:17 +0000

    some changes

--- trunk/forces.py
+++ trunk/forces.py
@@ -2,6 +2,10 @@
     A generic force class ,and several force objects that operate on particle
     systems.
 
+    Force -- A generic pairwise force that can be instantiated but
+            does nothing.
+
+
     Andrew Charles
 """
 
@@ -11,101 +15,96 @@ import scipy
 import neighbour_list
 
 # CONSTANTS
-CUTOFF_RADIUS = 10
-DIM = 2
-COLLISION_RADIUS_SQ = 1.0
-VACUUM_VISCOSITY = 0.1
-spring_k = 1.1
-rest_distance =  0.2
+#CUTOFF_RADIUS = 10
+#DIM = 2
+#COLLISION_RADIUS_SQ = 1.0
+#VACUUM_VISCOSITY = 0.1
+#spring_k = 1.1
+#rest_distance =  0.2
 
 class Force:
-    """ A generic pairwise particle force, between two particle
+    """ A generic pairwise particle force
         systems. The assumption is that the force is mutual. 
-        
         p1: The first particle set
-        p2: The second particle set
         nl: list of interacting pairs (p1,p2)
 
-        The design is wrong here...
+        This will become a subclass of Controller
 
     """ 
 
-    def __init__(self,particles1,particles2,nl):
-        self.p1 = particles1
-        self.p2 = particles2
+    def __init__(self,particles,nl,cutoff=100.0):
+        self.p = particles
         self.nl = nl
+        self.cutoff = cutoff
+        self.cutoffsq = cutoff*cutoff
     
     def apply(self):
         """ Apply the force to all particles in the nlist """
         for k in range(self.nl.nip):
-            self.apply_force(k)
+            if self.nl.rij[k]**2 <= self.cutoffsq:
+                self.apply_force(k)
+
+#CUTOFF_RADIUS = 10
+#DIM = 2
+#COLLISION_RADIUS_SQ = 1.0
+#VACUUM_VISCOSITY = 0.1
+#spring_k = 1.1
+#rest_distance =  0.2
 
 
 class HookesForce(Force):
-    def __init__(self,particles1,particles2,nl):
-        self.p1 = particles1
-        self.p2 = particles2
-        self.nl = nl
-        self.nl.cutoff_radius_sq = CUTOFF_RADIUS**2 
+    """
+         Magnitude of force is k(|r-ro|)
+         Assumes drij has been calculated and stored in nlist
+         Takes a particle p and an nlist pair reference k
+    """
+
+    def __init__(self,particles,neighbour_list,cutoff=50.0,k=1.0,w=5.0):
+        Force.__init__(self,particles,neighbour_list,cutoff=cutoff)
+        self.w = w
+        self.k = k
 
     def apply_force(self,k):
-        """ Takes a particle p and an nlist pair reference k
-        """
-        # magnitude of force is k(|r-ro|)
-        # should calculate this in nlist allocation
-        # and use the stored value
-        # actually, should calc it in a seperate method so
-        # we can get new distances without getting a new
-        # nlist completely
         i = self.nl.iap[k,0]
         j = self.nl.iap[k,1]
-        p1 = self.p1
-        p2 = self.p2
+        p = self.p
 
         drx =  self.nl.drij[k,0] 
         dry =  self.nl.drij[k,1]
         rdist = self.nl.rij[k]
         rsquared = rdist**2 
-        fmag = abs (spring_k * ( rdist - rest_distance ) )
+        fmag = abs (self.k * ( rdist - self.w ) )
             
         #resolve into components
         dvx = fmag * ( drx ) / rdist
         dvy = fmag * ( dry ) / rdist
-        p1.vdot[i,0] += dvx
-        p1.vdot[i,1] += dvy
-        p2.vdot[j,0] += -dvx
-        p2.vdot[j,1] += -dvy
+        p.vdot[i,0] += dvx
+        p.vdot[i,1] += dvy
+        p.vdot[j,0] += -dvx
+        p.vdot[j,1] += -dvy
 
 
 class CollisionForce(Force):
-    
-    def __init__(self,particles1,particles2,nl):
-        self.p1 = particles1
-        self.p2 = particles2
-        self.nl = nl
-        #self.nl.build_nl_brute_force()
-        #self.nl.cutoff_radius_sq = COLLISION_RADIUS_SQ 
+   
+    def __init__(self,particles,neighbour_list,cutoff=2.0):
+        Force.__init__(self,particles,neighbour_list,cutoff=cutoff)
 
     def apply_force(self,k):
         """ A hard collision is just an instantanous force. More
             like an impulse maybe. Anyhow, it changes v directly.
             TODO: rollback and roll forward to get rid of sticky balls.
         """
-
-        if self.nl.rij[k]**2 > self.nl.cutoff_radius_sq:
-            #print "toofar"
-            return
+        debug = False
         i = self.nl.iap[k,0]
         j = self.nl.iap[k,1]
-        p1 = self.p1
-        p2 = self.p2
-        debug = False 
-        # Calc v dot r, magrhosq, and others
-        dr = self.nl.drij[k]
+        p = self.p
         
+        dr = self.nl.drij[k]
         drsq = dr[0]**2 + dr[1]**2
-        vidotr = p1.v[i,0]*dr[0] + p1.v[i,1]*dr[1]
-        vjdotr = (p2.v[j,0]*dr[0] + p2.v[j,1]*dr[1])
+      
+        # Divergence
+        vidotr = p.v[i,0]*dr[0] + p.v[i,1]*dr[1]
+        vjdotr = (p.v[j,0]*dr[0] + p.v[j,1]*dr[1])
 
         # If the particles are moving away from each other do nothing
         if (vidotr < 0) and (vjdotr >  0):
@@ -115,42 +114,41 @@ class CollisionForce(Force):
         # of velocity
         vtix = (vidotr/drsq) * dr[0] 
         vtiy = (vidotr/drsq) * dr[1]
-        vnix = p1.v[i,0] - vtix
-        vniy = p1.v[i,1] - vtiy
+        vnix = p.v[i,0] - vtix
+        vniy = p.v[i,1] - vtiy
 
         vtjx = (vjdotr/drsq) * dr[0] 
         vtjy = (vjdotr/drsq) * dr[1]
-        vnjx = p2.v[j,0] - vtjx
-        vnjy = p2.v[j,1] - vtjy
+        vnjx = p.v[j,0] - vtjx
+        vnjy = p.v[j,1] - vtjy
 
         if debug:
-            vmagsqi = p1.v[i,0]**2 + p2.v[i,1]**2
-            vmagsqj = p1.v[j,0]**2 + p2.v[j,1]**2
+            vmagsqi = p.v[i,0]**2 + p.v[i,1]**2
+            vmagsqj = p.v[j,0]**2 + p.v[j,1]**2
             vmagsqrot = (vtix+vnix)**2 + (vtiy+vniy)**2
             mom_i = math.sqrt(vmagsqi)
             mom_j = math.sqrt(vmagsqj)
             print "Before"
             print mom_i,mom_j,mom_i+mom_j
-            #print vmagsqreg,vmagsqrot
 
         # Transfer tangential component of momentum
         tmp = vtix
-        vtix = vtjx * (p2.m[j]/p1.m[i])
-        vtjx = tmp * (p1.m[i]/p2.m[j])
+        vtix = vtjx * (p.m[j]/p.m[i])
+        vtjx = tmp * (p.m[i]/p.m[j])
         
         tmp = vtiy
-        vtiy = vtjy * (p2.m[j]/p1.m[i])
-        vtjy = tmp * (p1.m[i]/p2.m[j])
+        vtiy = vtjy * (p.m[j]/p.m[i])
+        vtjy = tmp * (p.m[i]/p.m[j])
 
         # Convert back to xy frame
-        p1.v[i,0] = vtix + vnix 
-        p1.v[i,1] = vtiy + vniy
-        p2.v[j,0] = vtjx + vnjx
-        p2.v[j,1] = vtjy + vnjy
+        p.v[i,0] = vtix + vnix 
+        p.v[i,1] = vtiy + vniy
+        p.v[j,0] = vtjx + vnjx
+        p.v[j,1] = vtjy + vnjy
         
         if debug:
-            vmagsqi = p1.v[i,0]**2 + p1.v[i,1]**2
-            vmagsqj = p2.v[j,0]**2 + p2.v[j,1]**2
+            vmagsqi = p.v[i,0]**2 + p.v[i,1]**2
+            vmagsqj = p.v[j,0]**2 + p.v[j,1]**2
             mom_i = math.sqrt(vmagsqi)
             mom_j = math.sqrt(vmagsqj)
             print "After"
@@ -160,9 +158,8 @@ class CollisionForce(Force):
 
 class SpamForce(Force):
     
-    def __init__(self,particles,nl):
-        self.p = particles
-        self.nl = nl
+    def __init__(self,particles,neighbour_list,cutoff=5.0):
+        Force.__init__(self,particles,neighbour_list,cutoff=cutoff)
 
     def apply_force(self,k):
         """ Calculates spam interaction between two particles.
@@ -191,22 +188,19 @@ class SpamForce(Force):
 
 
 class CohesiveSpamForce(Force):
-    
-    def __init__(self,particles,nl):
-        self.p = particles
-        self.nl = nl
-        #self.nl.cutoff_radius_sq = CUTOFF_RADIUS**2
+   
+    def __init__(self,particles,neighbour_list,cutoff=10.0):
+        Force.__init__(self,particles,neighbour_list,cutoff=cutoff)
 
     def apply_force(self,k):
         """ Calculates spam interaction between two particles.
             The spam density must have already been calculated.
         """
-        #def spam(p,i,j,dwdx):
+        p = self.p
         i = self.nl.iap[k,0]
         j = self.nl.iap[k,1]
-        pri = self.p.pco[i]
-        prj = self.p.pco[j]
-        p = self.p
+        pri = p.pco[i]
+        prj = p.pco[j]
         dwdx = self.nl.dwij[k,:]
         dvx =  (pri/p.rho[i]**2 + prj/p.rho[j]**2) * dwdx[0]
         dvy =  (pri/p.rho[i]**2 + prj/p.rho[j]**2) * dwdx[1]
@@ -238,11 +232,9 @@ class Gravity(Force):
         - look ahead one frame for position?
 
     """
-    def __init__(self,particles1,particles2,nl):
-        self.p1 = particles1
-        self.p2 = particles2
-        self.nl = nl
-        self.g = 03.
+    def __init__(self,particles,neighbour_list,cutoff=100.0,g=1.0):
+        Force.__init__(self,particles,neighbour_list,cutoff=cutoff)
+        self.g = g
 
     def apply_force(self,k):
         """ Calculates spam interaction between two particles.
@@ -251,22 +243,21 @@ class Gravity(Force):
         i = self.nl.iap[k,0]
         j = self.nl.iap[k,1]
 
-        p1 = self.p1
-        p2 = self.p2
+        p = self.p
 
         drx =  self.nl.drij[k,0] 
         dry =  self.nl.drij[k,1]
         rdist = self.nl.rij[k]
         rsquared = rdist**2 
 
-        fmag = self.g*p1.m[i]*p2.m[j]/rsquared
+        fmag = self.g*p.m[i]*p.m[j]/rsquared
 
         #resolve into components
         dvx = fmag * ( drx ) / rdist
         dvy = fmag * ( dry ) / rdist
-        p1.vdot[i,0] += dvx
-        p1.vdot[i,1] += dvy
-        p2.vdot[j,0] += -dvx
-        p2.vdot[j,1] += -dvy
+        p.vdot[i,0] += dvx
+        p.vdot[i,1] += dvy
+        p.vdot[j,0] += -dvx
+        p.vdot[j,1] += -dvy
         
 
